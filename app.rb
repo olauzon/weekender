bc3252c2b642e65a23c43c7a7a3dd81d66a680f1
Olivier Lauzon <olauzon@gmail.com>
2009-08-20 00:48:42 -0400

    Cleaned Haml

--- app.rb
+++ app.rb
@@ -9,29 +9,18 @@ get '/' do
 %h1.title weekender
 %p This example shows a limited set of options.
 %div.options
-  %form(method=\"get\" action=\"/\")
+  %form{:method => 'get', :action => '/'}
+    - ['year', 'month', 'day', 'before', 'after'].each do |method|
+    - id    = 'options_' + method
+    - name  = 'options[' + method + ']'
+    - value = @weekender.send(method.to_sym)
+    - label = (method == 'before' || method == 'after') ? 'number of weeks ' + method : method
+      %p
+        %label{:for => id}= label
+        %br
+        %input{:type => 'text', :name => name, :id => id, :value => value}
     %p
-      %label(for=\"options_year\") year
-      %br
-      %input(type=\"text\" name=\"options[year]\" id=\"options_year\" value=\"#{@weekender.year}\")
-    %p
-      %label(for=\"options_month\") month
-      %br
-      %input(type=\"text\" name=\"options[month]\" id=\"options_month\" value=\"#{@weekender.month}\")
-    %p
-      %label(for=\"options_day\") day
-      %br
-      %input(type=\"text\" name=\"options[day]\" id=\"options_day\" value=\"#{@weekender.day}\")
-    %p
-      %label(for=\"options_before\") number of weeks before
-      %br
-      %input(type=\"text\" name=\"options[before]\" id=\"options_before\" value=\"#{@weekender.before}\")
-    %p
-      %label(for=\"options_after\") number of weeks after
-      %br
-      %input(type=\"text\" name=\"options[after]\" id=\"options_after\" value=\"#{@weekender.after}\")
-    %p
-      %input(type=\"submit\" value=\"submit\")
+      %input{:type => 'submit', :value => 'submit'}
     %div.weekender= @weekender.display
 "
 end
@@ -40,7 +29,7 @@ template :layout do
 "
 !!! XML
 !!!
-%html(xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"en\" xml:lang=\"en\")
+%html{:xmlns => 'http://www.w3.org/1999/xhtml', :lang => 'en', 'xml:lang' => 'en'}
   %head
     %title weekender
   %body
